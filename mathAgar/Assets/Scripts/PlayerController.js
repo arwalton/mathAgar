d021c8b983aa3c508c3a0ddfc67efb55bbeb14f2
arwalton <adamrwalton@gmail.com>
2017-01-27 21:22:15 +0200

    Placeholders Movement
    Added a placeholder bg, a dot to represent the player, and he can move
    around while the camera follows.

---
+++ mathAgar/Assets/Scripts/PlayerController.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+public var speed : float; //Floating point to store player mvmt speed
+
+private var rb : Rigidbody2D;
+
+
+function Start () {
+	rb = GetComponent.<Rigidbody2D>();
+	speed = 5;
+}
+
+function FixedUpdate () {
+	var moveHorizontal = Input.GetAxis ("Horizontal");
+
+	var moveVertical = Input.GetAxis("Vertical");
+
+	var movement = new Vector2(moveHorizontal, moveVertical);
+
+	rb.AddForce (movement * speed);
+}
+
+
