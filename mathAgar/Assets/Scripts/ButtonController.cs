0dbfbc5b36b5f10cf24bb1903d49d1d3cfc74655
arwalton <adamrwalton@gmail.com>
2017-02-09 21:53:05 +0200

    The answer panel now displays numbers
    I've got most of the UI stuff programmed, so that it will display
    properly and do what you want when you click the buttons. I'm still
    working on making it actually work when you hit the enter key. It's
    almost there.

---
+++ mathAgar/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class ButtonController : MonoBehaviour {
+
+	public Text answerText;
+	public Text pIntText;
+	public Text operText;
+	public Text eIntText;
+	public bool isCorrect;
+
+	public void updateAnswer(){
+		string buttonText = EventSystem.current.currentSelectedGameObject.name;
+		if (buttonText == "Delete") {
+			if (answerText.text != "0")
+				answerText.text = answerText.text.Substring (0, answerText.text.Length - 1);
+
+			if (answerText.text.Length == 0) {
+				answerText.text = "0";
+			}
+		} else {
+			if (answerText.text == "0") {
+				answerText.text = buttonText;
+			} else {
+				answerText.text = answerText.text + buttonText;
+			}
+		}
+	}
+
+	public void checkAnswer(){
+		string oper = operText.text;
+		int answer = Convert.ToInt32(answerText.text);
+		int pInt = Convert.ToInt32(pIntText.text);
+		int eInt = Convert.ToInt32(eIntText.text);
+
+		switch (oper)	{
+		case "-":
+			if (answer == pInt - eInt) {
+				isCorrect = true;
+			}
+			break;
+		case "*":
+			if (answer == pInt * eInt) {
+				isCorrect = true;
+			}
+			break;
+		case "/":
+			if (answer == pInt / eInt) {
+				isCorrect = true;
+			}
+			break;
+		case "+": 
+		default:
+			if (answer == pInt + eInt) {
+				isCorrect = true;
+			}
+			break;
+		}
+	}
+}
